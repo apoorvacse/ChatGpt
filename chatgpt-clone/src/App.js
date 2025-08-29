e7ccbdb8477e31f89f942983c653e3f9b7558897
Apoorva Singh <129685480+apoorvacse@users.noreply.github.com>
2025-08-29 20:31:41 +0000

    add on

--- chatgpt-clone/src/App.js
+++ chatgpt-clone/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
-
+import gptlogo from './assets/chatgpt.svg';
 function App() {
   return (
     <div className="App">
-     
+      <div className="sideBar">
+        <div className="upperSide">
+           <div className="upperSideTop">
+            <img src={gptlogo} alt="LOGO" className="logo"/>
+            <span className="brand">
+              Chatgpt
+            </span></div>
+            <button className="midbtn"> <img src="" alt="" className="addbtn"/>New Chat</button>
+           <div className="upperSidebottom">
+            <button className="query"><img src="" alt=""/> what is programming?</button>
+             <button className="query"><img src="" alt=""/> what is programming?</button>
+
+           </div>
+        </div>
+        <div className="lowerSide">
+
+        </div>
+
+
+      </div>
     </div>
   );
 }
